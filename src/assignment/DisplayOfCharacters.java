33406479ec7802bc379f16dba6ba9f0368459e81
Muyiwa Odukoya <kayusyiwex@gmail.com>
2023-09-23 13:01:31 +0100

    commit message 23/9/2023

---
+++ src/assignment/DisplayOfCharacters.java
@@ -0,0 +1,21 @@
+package assignment;
+
+public class DisplayOfCharacters {
+    public static void main(String[] args) {
+
+        for(int number = 0; number <5; number ++){
+            System.out.println();
+            for(int numberTwo = 0; numberTwo <number + 1; numberTwo ++){
+                System.out.print("* ");
+        }
+
+    }
+        for(int number = 0; number <5; number ++){
+            System.out.println();
+            for(int numberTwo = 0; numberTwo <3 - number + 1; numberTwo ++){
+                System.out.print("* ");
+            }
+
+        }
+}
+}
